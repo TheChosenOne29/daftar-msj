8ff8afc7ad3ce7d1079ebbbc2e0dbd6ba20adc54
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-04-04 03:29:08 +0700

    added views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.main')
+
+@section('title')
+    Pendaftaran MSJ GMS Lampung
+@endsection
+
+@section('content')
+<div class="p-5 mb-4 border rounded-3 bg-light">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Shalom!</h1>
+        <p class="col-md-8 fs-4">Selamat datang para calon peserta My Spiritual Journey.
+            Silahkan pilih kelas MSJ yang ingin diikuti dibawah ini untuk mendapatkan informasi lebih lanjut.</p>
+    </div>
+</div>
+
+<div class="row align-items-md-stretch">
+    <div class="col">
+        <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>My Spiritual Journey 1</h2>
+            <p>MSJ 1 ditujukan untuk jiwa baru atau jemaat yang baru bergabung dalam Gereja Mawar Sharon.</p>
+            <button class="btn btn-primary" type="button">Daftar MSJ 1</button>
+        </div>
+    </div>
+    <div class="col">
+        <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>My Spiritual Journey 2</h2>
+            <p>MSJ 2 ditujukan untuk jemaat yang sudah bertumbuh di Gereja Mawar Sharon dan akan 
+                terlibat dalam pelayanan baik dalam departemen maupun penggembalaan.</p>
+            <button class="btn btn-primary" type="button">Daftar MSJ 2</button>
+        </div>
+    </div>
+    <div class="col">
+        <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>My Spiritual Journey 3</h2>
+            <p>MSJ 3 ditujukan untuk para pemimpin/calon pemimpin yang akan melayani 
+                baik dalam departemen maupun penggembalaan yang ada.</p>
+            <button class="btn btn-primary" type="button">Daftar MSJ 3</button>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="https://gmschurch-assets-2.b-cdn.net/gms-v2-white.png"
+                class=logo_main alt="GMS" height="24">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link {{ (request()->is('dashboard')) ? 'active' : '' }}" href="/dashboard">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ (request()->is('volunteer')) ? 'active' : '' }}" href="/volunteer">Volunteer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ (request()->is('schedule')) ? 'active' : '' }}" href="/schedule">Schedule</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
