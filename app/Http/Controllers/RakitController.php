2fdb41fb8f18d3ffc1db89c7668315b7846d008a
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-04-04 03:29:35 +0700

    rewrite controller, views for msj 1

---
+++ app/Http/Controllers/RakitController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RakitController extends Controller
+{
+    public function aktivasi()
+    {
+        return view("msj1.aktivasi");
+    }
+}
---
+++ resources/views/msj1/aktivasi.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('title')
+    Pendaftaran MSJ GMS Lampung
+@endsection
+
+@section('content')
+
+@endsection
