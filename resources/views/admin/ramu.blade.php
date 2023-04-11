8220460e5c385654501c1fd725f64b1a76ca3052
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-04-11 16:34:31 +0700

    fixed table inline editing

--- resources/views/admin/ramu.blade.php
+++ resources/views/admin/ramu.blade.php
@@ -10,47 +10,49 @@
 
 @section('content')
     <div class="container">
-        <div class="col-md-6 mx-auto">
+        <div class="col-md-9 mx-auto">
             <h4 style="text-align: center" class="mb-4">Jadwal MSJ 1</h4>
-            <table class="table table-bordered text-center">
-                <thead>
-                    <tr>
-                        <th scope="col">Kelas</th>
-                        <th scope="col">Tanggal</th>
-                        <th scope="col">Jam</th>
-                        <th scope="col">Judul</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($ramu as $item2)
-                        <form action="/admin/msj1/update/{{ $item2->id }}" method="POST">
-                            @csrf
-                            <tr>
-                                <th scope="row">
-                                    {{ $item2->id }}
-                                    <input type="hidden" name="id" value={{ $item2 -> id }}>
-                                </th>
-                                <td>
-                                    <input type="date" data-date-inline-picker="true" value="{{ $item2->tanggal }}"
-                                        name="tanggal" />
-                                </td>
-                                <td>
-                                    <input type="time" name="jam" value="{{ substr($item2->jam, 0, -3) }}">
-                                </td>
-                                <td>
-                                    <input type="text" name="judul" value="{{ $item2->judul }}">
-                                </td>
-                                <td>
-                                    <button type="submit" class="btn btn-sm btn-warning">
-                                        Update
-                                    </button>
-                                </td>
-                            </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </form>
+            <div class="table-responsive">
+                <table class="table table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Kelas</th>
+                            <th scope="col">Tanggal</th>
+                            <th scope="col">Jam</th>
+                            <th scope="col">Judul</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($ramu as $item2)
+                            <form action="/admin/msj1/update/{{ $item2->id }}" method="POST">
+                                @csrf
+                                <tr>
+                                    <th scope="row">
+                                        {{ $item2->id }}
+                                        <input type="hidden" name="id" value={{ $item2 -> id }}>
+                                    </th>
+                                    <td>
+                                        <input type="date" data-date-inline-picker="true" value="{{ $item2->tanggal }}"
+                                            name="tanggal" />
+                                    </td>
+                                    <td>
+                                        <input type="time" name="jam" value="{{ substr($item2->jam, 0, -3) }}">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="judul" style="width: 20em" value="{{ $item2->judul }}">
+                                    </td>
+                                    <td>
+                                        <button type="submit" class="btn btn-sm btn-warning">
+                                            Update
+                                        </button>
+                                    </td>
+                                </tr>
+                            </form>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             @if (session()->has('success-update'))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     <strong>Success!</strong> {{ session('success-update') }}
