60e467572d94ec195cff8ac7ff07abbdace16e73
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-04-11 18:18:19 +0700

    fixed app locale and timezone

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        {
+            config(['app.locale' => 'id']);
+            Carbon::setLocale('id');
+        }
     }
 }
