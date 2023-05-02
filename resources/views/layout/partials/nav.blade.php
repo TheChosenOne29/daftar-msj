ec0485a2120b2c4d28273621d993e7e5e1884f2e
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-05-03 02:54:37 +0700

    changed navbar

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -11,13 +11,10 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link {{ (request()->is('dashboard')) ? 'active' : '' }}" href="/dashboard">Dashboard</a>
+                    <a class="nav-link {{ (request()->is('booking')) ? 'active' : '' }}" href="/booking">Booking Ruang</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ (request()->is('volunteer')) ? 'active' : '' }}" href="/volunteer">Volunteer</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ (request()->is('schedule')) ? 'active' : '' }}" href="/schedule">Schedule</a>
+                    <a class="nav-link {{ (request()->is('schedule')) ? 'active' : '' }}" href="/schedule">Jadwal MSJ</a>
                 </li>
             </ul>
         </div>
